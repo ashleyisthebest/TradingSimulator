5bb5d7b644dc1fcae365b0bc4a3546f67e5ea722
ohlynx <ashleybest02@gmail.com>
2019-08-07 15:44:32 +0100

    First Commit

---
+++ TradingSimulator/src/tradingsimulator/TradingSimulator.java
@@ -0,0 +1,12 @@
+
+package tradingsimulator;
+
+public class TradingSimulator {
+
+    public static void main(String[] args) {
+        //Opens the login window
+        LoginWindow lw = new LoginWindow();
+        lw.setVisible(true);  
+    }
+    
+}
